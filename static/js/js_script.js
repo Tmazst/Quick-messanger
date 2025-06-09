f4caf8b7a215864353bfc17d2e96e24fa2b0a447
Tmazst <55834770+Tmazst@users.noreply.github.com>
2025-06-09 11:11:32 +0200

    download icon

--- static/js/js_script.js
+++ static/js/js_script.js
@@ -271,7 +271,7 @@ paragraph.forEach(function(pTag){
 //    console.log('Mouse Over');
 //}
 var container = document.querySelector(".app-dowload-container");
-// var containerTwo = document.querySelector(".app-install-prmpt");
+var containerTwo = document.querySelector(".app-dowload-container2");
 // var noBgBtn = document.querySelector(".no-bg-btn");
 // var installBtn = document.querySelector("#pwa-install-btn");
 // Check if the browser supports service workers and PWA installation
@@ -299,6 +299,7 @@ window.addEventListener('load', () => {
                     e.preventDefault();
                     deferredPrompt = e;
                     container.style.display = "flex";
+                    containerTwo.style.display = "flex";
                     // containerTwo.style.display = "flex";
                     const installBtn = document.getElementById('pwa-install-btn');
                     var sidenavBgBtn = document.getElementById("download-icon-2");
